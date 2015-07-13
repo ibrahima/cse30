ae0bcee51cf1eef8edb28a5544797c8d8915b73d
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-07-13 12:44:45 -0700

    Add some branch examples

---
+++ disc3/src/branch.c
@@ -0,0 +1,9 @@
+int main(){
+  int r = 3;
+  if(r > 2){
+    r = r + 1;
+  }else{
+    r = r - 1;
+  }
+  return 0;
+}
---
+++ disc3/src/branch_opt.s
@@ -0,0 +1,26 @@
+	.arch armv6
+	.eabi_attribute 27, 3
+	.eabi_attribute 28, 1
+	.fpu vfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 2
+	.eabi_attribute 18, 4
+	.file	"branch.c"
+	.section	.text.startup,"ax",%progbits
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	mov	r0, #0
+	bx	lr
+	.size	main, .-main
+	.ident	"GCC: (Debian 4.6.3-14+rpi1) 4.6.3"
+	.section	.note.GNU-stack,"",%progbits

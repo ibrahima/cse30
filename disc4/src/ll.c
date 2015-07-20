00b100f255a062e725af23ade89e221851e55abd
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-07-20 16:56:18 -0700

    Set up Linked list exercise ARM assembly file

--- disc4/src/ll.c
+++ disc4/src/ll.c
@@ -2,50 +2,6 @@
 #include <stdlib.h>
 #include "ll.h"
 
-Node *newNode(int val){
-    Node *n = malloc(sizeof(Node));
-    n->val = val;
-    n->next = NULL;
-    return n;
-}
-
-Node *insertNext(Node *n, int val){
-    Node *next = newNode(val);
-    if(n->next != NULL){
-        next->next = n->next;
-    }
-    n->next = next;
-    return next;
-}
-
-Node *append(Node *n, int val){
-    if(n->next == NULL){
-        return insertNext(n, val);
-    }else{
-        return append(n->next, val);
-    }
-}
-
-void printList(Node *start){
-    printf("%d ", start->val);
-    if(start->next != NULL){
-        printList(start->next);
-    }
-}
-
-int removeVal(Node *n, int val){
-    if(n->next == NULL){
-        return -1;
-    }else{
-        if(n->next->val == val){
-            n->next = n->next->next;
-            return 0;
-        }else{
-            return removeVal(n->next, val);
-        }
-    }
-}
-
 int main(){
     Node *a = newNode(1);
     append(a, 5);
--- disc4/src/ll_arm.s
+++ disc4/src/ll_arm.s
@@ -1,10 +1,68 @@
-.func get_min_ARM, get_min_ARM
-.type get_min_ARM, %function
+.syntax unified
+.text
+.align 8
 
-get_min_ARM:
+@ Node *newNode(int val)
+.global newNode
+.func newNode, newNode
+.type newNode, %function
+
+newNode:
     push {r4-r11, ip, lr} @@ Save caller's registers on the stack
     @@ put your return value in r0
-return:
+return1:
+    pop {r4-r11, ip, lr} @@ restore caller's registers
+    BX lr
+.endfunc
+
+@ Node *insertNext(Node *n, int val)
+.global insertNext
+.func insertNext, insertNext
+.type insertNext, %function
+
+insertNext:
+    push {r4-r11, ip, lr} @@ Save caller's registers on the stack
+    @@ put your return value in r0
+return2:
+    pop {r4-r11, ip, lr} @@ restore caller's registers
+    BX lr
+.endfunc
+
+@ Node *append(Node *n, int val)
+.global append
+.func append, append
+.type append, %function
+
+append:
+    push {r4-r11, ip, lr} @@ Save caller's registers on the stack
+    @@ put your return value in r0
+return3:
+    pop {r4-r11, ip, lr} @@ restore caller's registers
+    BX lr
+.endfunc
+
+@ void printList(Node *start)
+.global printList
+.func printList, printList
+.type printList, %function
+
+printList:
+    push {r4-r11, ip, lr} @@ Save caller's registers on the stack
+    @@ put your return value in r0
+return4:
+    pop {r4-r11, ip, lr} @@ restore caller's registers
+    BX lr
+.endfunc
+
+@ int *removeVal(Node *n, int val)
+.global removeVal
+.func removeVal, removeVal
+.type removeVal, %function
+
+removeVal:
+    push {r4-r11, ip, lr} @@ Save caller's registers on the stack
+    @@ put your return value in r0
+return5:
     pop {r4-r11, ip, lr} @@ restore caller's registers
     BX lr
 .endfunc

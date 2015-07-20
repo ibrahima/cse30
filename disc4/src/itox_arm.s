1da1da8387bd191363e38ef4bda1084be6793a7d
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-07-20 03:37:24 -0700

    Add some starter code

--- disc4/src/itox_arm.s
+++ disc4/src/itox_arm.s
@@ -8,6 +8,14 @@
 itohex:
     push {r4-r11, ip, lr} @@ Save caller's registers on the stack
     @@ put your return value in r0
+    mov r0, #11
+    mov r4, #0
+    bl malloc @ r0 contains our pointer
+    strb r4, [r0, #10]
+    mov r4, '0'
+    strb r4, [r0, #0]
+    mov r4, 'x'
+    strb r4, [r0, #1]
 return:
     pop {r4-r11, ip, lr} @@ restore caller's registers
     BX lr

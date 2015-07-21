9bbbe024a2654325b95c55c153468e99f1120485
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2015-07-21 02:22:02 -0700

    Update tree ARM assembly so that it compiles.

--- disc4/src/tree_arm.s
+++ disc4/src/tree_arm.s
@@ -1,10 +1,48 @@
-.func get_min_ARM, get_min_ARM
-.type get_min_ARM, %function
+.syntax unified
+.text
+.align 8
 
-get_min_ARM:
+@@ TreeNode *newTreeNode(int val);
+.func newTreeNode, newTreeNode
+.type newTreeNode, %function
+newTreeNode:
     push {r4-r11, ip, lr} @@ Save caller's registers on the stack
     @@ put your return value in r0
-return:
+return1:
+    pop {r4-r11, ip, lr} @@ restore caller's registers
+    BX lr
+.endfunc
+
+@@ TreeNode *insert(TreeNode *n, int val);
+.func insert, insert
+.type insert, %function
+insert:
+    push {r4-r11, ip, lr} @@ Save caller's registers on the stack
+    @@ put your return value in r0
+return2:
+    pop {r4-r11, ip, lr} @@ restore caller's registers
+    BX lr
+.endfunc
+
+@@ void printInOrder(TreeNode *n);
+.func printInOrder, printInOrder
+.type printInOrder, %function
+
+printInOrder:
+    push {r4-r11, ip, lr} @@ Save caller's registers on the stack
+    @@ put your return value in r0
+return3:
+    pop {r4-r11, ip, lr} @@ restore caller's registers
+    BX lr
+.endfunc
+
+@@ int removeVal(TreeNode *n, int val);
+.func removeVal, removeVal
+.type removeVal, %function
+removeVal:
+    push {r4-r11, ip, lr} @@ Save caller's registers on the stack
+    @@ put your return value in r0
+return4:
     pop {r4-r11, ip, lr} @@ restore caller's registers
     BX lr
 .endfunc
